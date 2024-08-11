8fb79b027ce5975ee0b3ce307889dd98f72d0f7d
advaitmenon1106 <advaitmenon1106@gmail.com>
2024-08-11 22:44:36 +0530

    Update the date parser code.

--- Lab-3/DateParser.py
+++ Lab-3/DateParser.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 def parse_dates(sentences):
     date_formats = [
-        r'\d{4}-\d{2}-\d{2}',
-        r'\d{2}/\d{2}/\d{4}',
+        r'(\d{1,2})\W+(\d{1,2})\W+(\d{4})',
+        r'(\d{4})\W+(\d{1,2})\W+(\d{1,2})',
         r'(January|February|March|April|May|June|July|August|September|October|November|December)\s+\d{1,2},\s+\d{4}'
     ]
 
@@ -12,35 +12,38 @@ def parse_dates(sentences):
     for sentence in sentences:
         for f in date_formats:
             matches = re.findall(f, sentence)
-            dates.extend(matches)
+            # print(matches)
+            if len(matches) != 0: 
+                dates.extend(matches)
     return dates
 
 
 df = pd.read_csv('Lab-3/date_parser_testcases.csv')
 sentences = df['Input'].tolist()
 parsed = parse_dates(sentences)
-parsed_mid = []
-
-for d in parsed:
-    if '-' in d:
-        parsed_mid.append('/'.join(d.split('-')))
-    else:
-        parsed_mid.append(d)
 
 parsed_final = []
 
-for d in parsed_mid:
-    if '/' in d:
-        d = d.split('/')
-        for val in d:
-            if int(d[1])>int(d[2]):
-                temp = d[1]
-                d[1] = d[2]
-                d[2] = temp
-            parsed_final.append('/'.join(d))
+# for d in parsed:
+#     if '/' in d:
+#         d = d.split('/')
+#         for val in d:
+#             if int(d[1])>int(d[2]):
+#                 d[1], d[2] = d[2], d[1]
+#             parsed_final.append('/'.join(d))
             
-    else:
-        parsed_final.append(d)
+#     else:
+#         parsed_final.append(d)
 
-print(parsed_final)
-print(df['Expected Output'].tolist())
+print(parsed)
+
+
+
+print(len(parsed_final))
+
+
+# for i in range(0, 95):
+#     if df['Expected Output'].iloc[i] == parsed_final[i]:
+#         c+=1
+
+# print(c)
